fd85d536d109dd6521a87e33ae416d61c24bc6ef
kevinharv <kharvey2002@gmail.com>
2023-09-10 10:51:21 -0500

    Started search form

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
+import SearchCard from "@/components/SearchCard"
+
 
 export default function Home() {
   // if !isAuthenticated {
   //   // redirect to the login page
   // }
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className="flex flex-row justify-center">
+      <div className="border-2 p-2">
+        <h1 className="text-4xl font-bold underline">Directory Search</h1>
+        <SearchCard />
+      </div>
     </div>
   )
 }
--- components/SearchCard.tsx
+++ components/SearchCard.tsx
@@ -1,12 +1,15 @@
 
 export default function SearchCard() {
 
-
     return (
         <div>
             <h1>User</h1>
             <h1>Group</h1>
             <h1>Computer</h1>
+            <form>
+                <input type="text" name="Test" />
+                <button type="submit">Submit</button>
+            </form>
         </div>
     );
 }
