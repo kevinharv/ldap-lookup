458fe64807000db1ea3448de69971a7669afc0e1
kevinharv <kharvey2002@gmail.com>
2023-11-17 20:05:00 -0600

    Started working on doing things correctly

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,27 +1,13 @@
 // GET request under user route
-import { Client, DN } from "ldapts";
-
-const SERVER = process.env.LDAP_SERVER;
-const BIND_DN = process.env.LDAP_BIND_DN;
-const BIND_PW = process.env.LDAP_BIND_PW;
+import { ADClient } from "@/utils/connect";
 
 export async function GET(request: Request) {
-    const AD = new Client({
-        url: `ldap://${SERVER}`
-    });
-    await AD.bind(`${BIND_DN}`, BIND_PW);
+    const AD = new ADClient();
+    
+    const bindStatus = await AD.bind();
 
-    // const { searchEntries, searchReferences } = await AD.search("OU=KEVHARV Users,DC=ad,DC=kevharv,DC=com", {
-    //     scope: "sub",
-    //     filter: '(sAMAccountName=kharvey)'
-    // });
+    const searchResults = await AD.searchUser("kharvey"); 
 
-    const { searchEntries, searchReferences } = await AD.search("CN=Schema,CN=Configuration,DC=ad,DC=kevharv,DC=com", {
-        scope: "sub",
-        sizeLimit: 6000,
-        attributes: ['cn']
-    });
-
-    await AD.unbind();
-    return Response.json({entries: searchEntries.length});
+    const unbindStatus = await AD.unbind();
+    return Response.json(searchResults);
 }
--- utils/connect.ts
+++ utils/connect.ts
@@ -6,23 +6,75 @@ import { Client } from "ldapts";
 
 // Disconnect?
 
+const SERVER = process.env.LDAP_SERVER;
+const BIND_DN = process.env.LDAP_BIND_DN;
+const BIND_PW = process.env.LDAP_BIND_PW;
+const SEARCH_DN = process.env.LDAP_SEARCH_DN;
 
 export class ADClient {
     client: any;
 
     constructor() {
         this.client = new Client({
-            url: `ldap://${process.env.LDAP_SERVER}`
+            url: `ldap://${SERVER}`
         });
     }
 
+    // Bind to LDAP Server
     async bind() {
-        await this.client.bind(process.env.LDAP_BIND_DN, process.env.LDAP_BIND_PW);
-        // console.debug(`AD Client Bound on DN: ${process.env.LDAP_BIND_DN}`);
-        return process.env.LDAP_BIND_DN;
+        try {
+            await this.client.bind(BIND_DN, BIND_PW);
+            return {
+                "status": "Success"
+            }
+        } catch (e) {
+            const debugInfo = {
+                "server": SERVER,
+                "bindDN": BIND_DN,
+                "error": e
+            }
+            console.error(`Failed to bind to LDAP server\n${debugInfo}`);
+            return {
+                "status": "Failure",
+                "debug": e
+            }
+        }
     }
 
+    // Unbind from LDAP Server
     async unbind() {
-        await this.client.unbind();
+        try {
+            await this.client.unbind();
+            return {
+                "status": "Success"
+            }
+        } catch (e) {
+            const debugInfo = {
+                "server": SERVER,
+                "bindDN": BIND_DN,
+                "error": e
+            }
+            console.error(`Failed to unbind to LDAP server\n${debugInfo}`);
+            return {
+                "status": "Failure",
+                "debug": e
+            }
+        }
+    }
+
+    async searchUser(searchTerm: string) {
+        const { searchEntries, searchReferences } = await this.client.search(SEARCH_DN, {
+                scope: "sub",
+                filter: `|(sAMAccountName=${searchTerm})(userPrincipalName=${searchTerm}*)`
+            });
+        return searchEntries;
+    }
+
+    async searchGroup(searchTerm: string) {
+
+    }
+
+    async searchComputer(searchTerm: string) {
+
     }
 }
